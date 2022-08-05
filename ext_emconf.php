885527974ba3fcd040cc76b676dc8a446da3bc6a
Elias Häußler <e.haeussler@familie-redlich.de>
2022-08-05 22:02:53 +0200

    [BUGFIX] Drop deprecated and superfluous configuration

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,9 +7,6 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'coding. powerful. systems. CPS GmbH',
     'author_email' => '	typo3-government-package@cps-it.de',
     'state' => 'alpha',
-    'internal' => '',
-    'uploadfolder' => 1,
-    'createDirs' => '',
     'clearCacheOnLoad' => 1,
     'version' => '0.0.3',
     'autoload' => [
@@ -22,9 +19,6 @@ $EM_CONF[$_EXTKEY] = [
             'news' => '9.4.0-0.0.0.',
             'bootstrap_package' => '12.0.0-0.0.0.',
             'rx_shariff' => '14.0.0-0.0.0.',
-        ],
-        'conflicts' => [],
-        'suggests' => [
         ],
     ],
 ];
